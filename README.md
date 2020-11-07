0c6c3e99498362671a9d48355154f8502616774c
jindw <jindaweihd@163.com>
2020-11-07 16:31:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+### s-cli
+
+#### 初始化
+> `s-cli init [name]`
+
+#### 动态生成`router.jsx`
+> `s-cli refresh`
---
+++ bin/s-cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const program = require('commander')
+const init = require('../lib/init')
+const refresh = require('../lib/refresh')
+
+program.version(require('../package.json').version)
+
+program
+    .command("init <name>")
+    .description("init project")
+    .action(init)
+
+program
+    .command("refresh")
+    .description("refresh")
+    .action(refresh)
+
+
+/**
+ * process执行的进程,
+ * argv 执行的参数
+ */
+program.parse(process.argv)
+
