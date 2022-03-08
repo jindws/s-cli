fe8c52aba2993f7573c8e533ba36cb744e3db72e
jindw <jindawei@youzan.com>
2022-03-08 22:56:17 +0800

    feat:pretty

--- bin/s-cli.js
+++ bin/s-cli.js
@@ -1,24 +1,16 @@
 #!/usr/bin/env node
-const program = require('commander')
-const init = require('../lib/init')
-const refresh = require('../lib/refresh')
+const program = require("commander");
+const init = require("../lib/init");
+const refresh = require("../lib/refresh");
 
-program.version(require('../package.json').version)
+program.version(require("../package.json").version);
 
-program
-    .command("init <name>")
-    .description("init project")
-    .action(init)
+program.command("init <name>").description("init project").action(init);
 
-program
-    .command("refresh")
-    .description("refresh")
-    .action(refresh)
-
+program.command("refresh").description("refresh").action(refresh);
 
 /**
  * process执行的进程,
  * argv 执行的参数
  */
-program.parse(process.argv)
-
+program.parse(process.argv);
